98fb0a77a6da88ac8037b588c479a5be1fe01971
Thandiswa25 <151035755+Thandiswa25@users.noreply.github.com>
2024-09-06 12:16:20 +0200

    Add files via upload

---
+++ Data/APPRContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APPR.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace APPR.Data
+{
+    public class APPRContext : IdentityDbContext
+    {
+        public APPRContext (DbContextOptions<APPRContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APPR.Models.Category> Category { get; set; } = default!;
+
+        public DbSet<APPR.Models.DisasDon>? DisasDon { get; set; }
+
+        public DbSet<APPR.Models.goodAlloc>? goodAlloc { get; set; }
+
+        public DbSet<APPR.Models.GoodDon>? GoodDon { get; set; }
+
+        public DbSet<APPR.Models.monAlloc>? monAlloc { get; set; }
+
+        public DbSet<APPR.Models.monDon>? monDon { get; set; }
+
+        public DbSet<APPR.Models.purchas>? purchas { get; set; }
+    }
+}
