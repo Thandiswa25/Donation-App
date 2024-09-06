2f97e755390f3b0c5e50db0b014748afcb29904f
Thandiswa25 <151035755+Thandiswa25@users.noreply.github.com>
2024-09-06 12:16:53 +0200

    Add files via upload

---
+++ MergeDonation/MergeDon.cs
@@ -0,0 +1,31 @@
+﻿using APPR.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace APPR.MergeDonation
+{
+    public class MergeDon
+    {
+        private readonly APPRContext _context;
+
+        public MergeDon(APPRContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> FinalCalculations()
+        {
+            var FinalMonetaryDonation = await _context.monDon.SumAsync(d => d.Amount);
+            var totalPurchaseCapturing = await _context.purchas.SumAsync(p => p.PurchaseAmount);
+
+            var finalCals = FinalMonetaryDonation - totalPurchaseCapturing;
+
+            return (int)finalCals;
+        }
+        public async Task<int> FinalGoodsCalculations()
+        {
+            var FinalGoodDonation = await _context.GoodDon.SumAsync(d => d.Numofitems);
+            
+            return (int)FinalGoodDonation;
+        }
+    }
+}
